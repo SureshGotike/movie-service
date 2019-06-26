d99a9fd1f93cb9307126ad0a3620ee71cace1f98
SXG6Wo7 <Samanvi123@>
2019-06-25 22:50:26 -0400

    docekr compose changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM openjdk:8
 ADD build/libs/movieservice-0.0.1-SNAPSHOT.jar movieservice.jar
-EXPOSE 8082
-ENTRYPOINT ["java", "-jar", "movieservice.jar"]
+ENTRYPOINT ["java", "-Dspring.data.mongodb.uri=mongodb://mongo:27017/mongodb", "-Djava.security.egd=file:/dev/./urandom","-jar", "movieservice.jar"]
--- src/main/java/com/movie/movieservice/MovieserviceApplication.java
+++ src/main/java/com/movie/movieservice/MovieserviceApplication.java
@@ -1,9 +1,11 @@
 package com.movie.movieservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MovieserviceApplication
 {
 
